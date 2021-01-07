174e5121ecdd5aab36bac21a6b7d97dd44841e19
hack_er <70893070+Er4ik@users.noreply.github.com>
2021-01-07 12:18:16 +0200

    Update

--- Study on Stepik/Количество совпадающих пар.py
+++
@@ -1,9 +0,0 @@
-string = input().split()
-counter = 0
-for i in range(len(string)):
-    for j in range(len(string)):
-        if i == j:
-            continue
-        if string[j] == string[i]:
-            counter += 1
-print(counter//2)
